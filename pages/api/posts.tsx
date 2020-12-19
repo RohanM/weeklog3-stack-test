f9e93b0024540f72a455293544c113e019fce14d
Rohan Mitchell <rohan@rohanmitchell.com>
2020-12-20 09:14:41 +1100

    Rename column title to message

--- pages/api/posts.tsx
+++ pages/api/posts.tsx
@@ -6,10 +6,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const prisma = new PrismaClient();
 
-    const title = req.body.post;
+    const message = req.body.post;
 
     const post = await prisma.post.create({
-      data: { title },
+      data: { message },
     });
 
     res.statusCode = 200;
