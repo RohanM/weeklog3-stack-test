da25d132e4784ac793ee91d87528fb269d9c4380
Rohan Mitchell <rohan@rohanmitchell.com>
2020-12-20 09:31:36 +1100

    Move chat interface to /chat/[name]

--- pages/chat/[name].tsx
+++ pages/chat/[name].tsx
@@ -1,13 +1,53 @@
-import { ReactElement } from 'react'
-import { useRouter } from 'next/router'
+import { ReactElement, useState } from 'react'
+import { GetServerSideProps } from 'next'
+//import { useRouter } from 'next/router'
+import Head from 'next/head'
 
-export default function Chat(): ReactElement {
-  const router = useRouter();
-  const { name } = router.query
+import { PrismaClient } from '@prisma/client'
+
+import styles from '../../styles/Home.module.css'
+
+import { Post } from '../../lib/types'
+
+import Posts from '../../components/Posts'
+import AddPost from '../../components/AddPost'
+
+export default function Chat({ posts: initialPosts }: { posts: Post[] }): ReactElement {
+  //const router = useRouter();
+  //const { name } = router.query
+
+  let [posts, setPosts] = useState(initialPosts);
+
+  const onPost = (post: Post) => {
+    setPosts(posts.concat(post));
+  };
 
   return (
-    <div>
-      Hello, {name}!
+    <div className={styles.container}>
+      <Head>
+        <title>Chat</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Chat
+        </h1>
+
+        <Posts posts={posts} />
+        <AddPost onPost={onPost} />
+      </main>
     </div>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const prisma = new PrismaClient();
+  const posts = (await prisma.post.findMany()).map(
+    post => ({ ...post, createdAt: post.createdAt.toString() })
+  );
+
+  return {
+    props: { posts }
+  };
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,21 @@
 import { ReactElement, useState } from 'react'
 
 import Head from 'next/head'
-import { GetServerSideProps } from 'next'
 import styles from '../styles/Home.module.css'
 
-import { Post } from '../lib/types'
-
-import Posts from '../components/Posts'
-import AddPost from '../components/AddPost'
-
-import { PrismaClient } from '@prisma/client'
-
-export default function Home({ posts: initialPosts }: { posts: Post[] }): ReactElement {
-  let [posts, setPosts] = useState(initialPosts);
-
-  const onPost = (post: Post) => {
-    setPosts(posts.concat(post));
-  };
-
+export default function Home(): ReactElement {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Chat</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Chat
         </h1>
-
-        <Posts posts={posts} />
-        <AddPost onPost={onPost} />
       </main>
     </div>
   )
-}
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const prisma = new PrismaClient();
-  const posts = (await prisma.post.findMany()).map(
-    post => ({ ...post, createdAt: post.createdAt.toString() })
-  );
-
-  return {
-    props: { posts }
-  };
 }
