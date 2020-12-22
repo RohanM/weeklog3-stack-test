82d01ef2c8a1de0b472554b6fce88566d176d1bc
Rohan Mitchell <rohan@rohanmitchell.com>
2020-12-23 09:15:39 +1100

    Add missing return type

--- pages/api/posts.tsx
+++ pages/api/posts.tsx
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import Posts from '../../lib/data/posts'
 
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse): void => {
   if (req.method === "POST") {
     const post = await Posts.create(req.body);
 
