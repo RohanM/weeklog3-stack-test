5b6ba403d61e8e9b49059bfa71a849fb5cfa7d72
Rohan Mitchell <rohan@rohanmitchell.com>
2020-12-23 14:49:37 +1100

    Strenghten home page spec

--- pages/index.spec.tsx
+++ pages/index.spec.tsx
@@ -1,14 +1,20 @@
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 
 import Home from './index'
-
 import EnterName from '../components/EnterName'
 
+const mockRouter = { push: jest.fn() };
+jest.mock('next/router', () =>
+  ({ useRouter: () => mockRouter })
+);
 
 describe("Home page", () => {
-  it("renders a field to enter your name", () => {
-    const wrapper = shallow(<Home />);
+  it("accepts my name then takes me to the chat", async () => {
+    const wrapper = mount(<Home />);
 
-    expect(wrapper.find(EnterName).length).toEqual(1);
+    wrapper.find(EnterName).find("input").simulate("change", { target: { value: "Montgomery" } });
+    wrapper.find(EnterName).find("form").simulate("submit");
+
+    expect(mockRouter.push).toHaveBeenCalledWith("/chat/Montgomery");
   });
 });
